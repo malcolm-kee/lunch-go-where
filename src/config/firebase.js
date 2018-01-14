748034186269c32701a01c643c2b769344f11020
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-01-14 13:06:14 +0800

    added firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyCY6EKrpnKtLXIfFFL2EEf25vc7NW0nXfA",
+  authDomain: "lunch-go-where-1514893968630.firebaseapp.com",
+  databaseURL: "https://lunch-go-where-1514893968630.firebaseio.com",
+  projectId: "lunch-go-where-1514893968630",
+  storageBucket: "lunch-go-where-1514893968630.appspot.com",
+  messagingSenderId: "555632314790"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+
+export const database = firebase.database();
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
